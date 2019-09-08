e3fdfec0c416cbdc5dc01d76590d7903e83da46c
guoshusong <g15176193717@gmail.com>
2019-09-08 21:44:29 +0800

    后端统一返回体

--- src/main/java/com/volunt/message/MessageApplication.java
+++ src/main/java/com/volunt/message/MessageApplication.java
@@ -1,9 +1,11 @@
 package com.volunt.message;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.volunt.message.dao")
 public class MessageApplication {
 
     public static void main(String[] args) {
