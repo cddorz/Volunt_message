b7a0635ece882e928c51e2dfb7421ef403485445
guoshusong <g15176193717@gmail.com>
2019-09-11 19:51:31 +0800

    优化Controller层和Service层代码

--- src/main/java/com/volunt/message/controller/MemMessage.java
+++ src/main/java/com/volunt/message/controller/MemMessage.java
@@ -19,32 +19,12 @@ public class MemMessage {
 
     @Resource
     MemberMessage memberMessage;
-//    @Resource
-//    UniversalResponseBody universalResponseBody;
 
 
     @PostMapping("/InsertMessage")
     public UniversalResponseBody Insert(@RequestParam("homeAddress") String homeAddress, @RequestParam("main_id")Integer main_id, @RequestParam(value = "birthday",required = false)Date birthday, @RequestParam("college")String college, @RequestParam("profession")String profession, @RequestParam("dormitory")String dormitory,@RequestParam("name")String name) {
 
-        try{
-            if (memberMessage.InsertMemberMess(homeAddress, college, profession, birthday, dormitory, main_id,name) != 0) {
-                Map map =new HashMap();
-                map.put("homeAddress",homeAddress);
-                map.put("main_id",main_id);
-                map.put("college",college);
-                map.put("profession",profession);
-                map.put("birthday",birthday);
-                map.put("dormitory",dormitory);
-                map.put("name",name);
-                return new UniversalResponseBody<>(201,"成功",map);
-
-            } else {
-                return new UniversalResponseBody<>(400,"失败",null);
-            }
-        }catch (Exception e){
-            System.out.println("成员信息录入发生错误");
-            return new UniversalResponseBody<>(400,"失败",null);
-        }
+         return  memberMessage.InsertMemberMess(homeAddress, college, profession, birthday, dormitory, main_id, name);
     }
 
 
--- src/main/java/com/volunt/message/service/MemberMessage.java
+++ src/main/java/com/volunt/message/service/MemberMessage.java
@@ -1,10 +1,13 @@
 package com.volunt.message.service;
 
 import com.volunt.message.dao.InsertMember;
+import com.volunt.message.tools.UniversalResponseBody;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
@@ -13,7 +16,25 @@ public class MemberMessage {
     InsertMember insertMember;
 
 
-    public int InsertMemberMess(String homeAddress, String college, String profession, Date birthday, String dormitory, Integer main_id,String name){
-        return insertMember.InsertMember(homeAddress,college,profession,birthday,dormitory,main_id,name);
+    public UniversalResponseBody InsertMemberMess(String homeAddress, String college, String profession, Date birthday, String dormitory, Integer main_id,String name){
+        try{
+            if (insertMember.InsertMember(homeAddress, college, profession, birthday, dormitory, main_id,name) != 0) {
+                Map map =new HashMap();
+                map.put("homeAddress",homeAddress);
+                map.put("main_id",main_id);
+                map.put("college",college);
+                map.put("profession",profession);
+                map.put("birthday",birthday);
+                map.put("dormitory",dormitory);
+                map.put("name",name);
+                return new UniversalResponseBody<>(201,"成功",map);
+
+            } else {
+                return new UniversalResponseBody<>(400,"失败",null);
+            }
+        }catch (Exception e){
+            System.out.println("成员信息录入发生错误");
+            return new UniversalResponseBody<>(400,"失败",null);
+        }
     }
 }
